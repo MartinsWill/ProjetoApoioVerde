06e43c08691031f572d36e91b369bc9a0435c93f
MartinsWill <69646505+MartinsWill@users.noreply.github.com>
2021-03-19 13:04:55 -0300

    ApoioVerde_v1.0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+
+    window.onscroll = function () {
+        if (window.pageYOffset > 140) {
+            $('#header').addClass('active')
+        } else {
+            $('#header').removeClass('active')
+        }
+
+    }
+});
+
